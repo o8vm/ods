eff41333b224568be14d2f7efb8f50bdc99822de
ellbrid <ell@exoskeleton.dev>
2020-05-31 14:34:07 +0900

    Implementing done and passed the test.

--- chapter09/src/redblacktree.rs
+++ chapter09/src/redblacktree.rs
@@ -120,26 +120,52 @@ impl<T: Ord + Clone> RedBlackTree<T> {
         u.color.swap(&w.color);
     }
     fn push_black(u: &Rc<RBTNode<T>>) {
-        *u.color.borrow_mut() = Color::Red;
-        u.left
-            .borrow()
-            .as_ref()
-            .map(|left| *left.color.borrow_mut() = Color::Black);
-        u.right
-            .borrow()
-            .as_ref()
-            .map(|right| *right.color.borrow_mut() = Color::Black);
+        let uc = *u.color.borrow() as isize - 1;
+        *u.color.borrow_mut() = match uc {
+            0 => Color::Red,
+            1 => Color::Black,
+            _ => Color::WBlack,
+        };
+        u.left.borrow().as_ref().map(|left| {
+            let lc = *left.color.borrow() as isize + 1;
+            *left.color.borrow_mut() = match lc {
+                0 => Color::Red,
+                1 => Color::Black,
+                _ => Color::WBlack,
+            };
+        });
+        u.right.borrow().as_ref().map(|right| {
+            let rc = *right.color.borrow() as isize + 1;
+            *right.color.borrow_mut() = match rc {
+                0 => Color::Red,
+                1 => Color::Black,
+                _ => Color::WBlack,
+            }
+        });
     }
     fn pull_black(u: &Rc<RBTNode<T>>) {
-        *u.color.borrow_mut() = Color::Black;
-        u.left
-            .borrow()
-            .as_ref()
-            .map(|left| *left.color.borrow_mut() = Color::Red);
-        u.right
-            .borrow()
-            .as_ref()
-            .map(|right| *right.color.borrow_mut() = Color::Red);
+        let uc = *u.color.borrow() as isize + 1;
+        *u.color.borrow_mut() = match uc {
+            0 => Color::Red,
+            1 => Color::Black,
+            _ => Color::WBlack,
+        };
+        u.left.borrow().as_ref().map(|left| {
+            let lc = *left.color.borrow() as isize - 1;
+            *left.color.borrow_mut() = match lc {
+                0 => Color::Red,
+                1 => Color::Black,
+                _ => Color::WBlack,
+            };
+        });
+        u.right.borrow().as_ref().map(|right| {
+            let rc = *right.color.borrow() as isize - 1;
+            *right.color.borrow_mut() = match rc {
+                0 => Color::Red,
+                1 => Color::Black,
+                _ => Color::WBlack,
+            }
+        });
     }
     fn flip_left(&mut self, u: &Rc<RBTNode<T>>) {
         Self::swap_colors(u, u.right.borrow().as_ref().unwrap());
@@ -242,97 +268,152 @@ impl<T: Ord + Clone> RedBlackTree<T> {
         self.n -= 1;
         Some(Rc::try_unwrap(u).ok().unwrap().x.into_inner())
     }
-    fn remove_fixup(&mut self, mut u: Rc<RBTNode<T>>) {
-        while *u.color.borrow() == Color::WBlack {
-            if Rc::ptr_eq(&u, self.r.as_ref().unwrap()) {
-                *u.color.borrow_mut() = Color::Black;
-            } else {
-                let w = u
-                    .parent
-                    .borrow()
-                    .as_ref()
-                    .and_then(|p| p.upgrade())
-                    .unwrap();
-                let left = w.left.borrow().clone();
-                match left {
-                    Some(ref left) if *left.color.borrow() == Color::Red => {
-                        u = self.remove_fix_case1(u)
+    fn remove_fixup(&mut self, mut color: isize, mut u: Tree<T>, mut p: Tree<T>) {
+        while color > 1 {
+            let n = u.clone();
+            match n {
+                Some(ref u) if Rc::ptr_eq(u, self.r.as_ref().unwrap()) => {
+                    *u.color.borrow_mut() = Color::Black;
+                    color = Color::Black as isize;
+                }
+                _ => {
+                    let left = p.as_ref().and_then(|p| p.left.borrow().clone());
+                    match left {
+                        Some(ref left) if *left.color.borrow() == Color::Red => {
+                            let result = self.remove_fix_case1(u, p, color);
+                            color = result.0;
+                            u = result.1;
+                            p = result.2;
+                        }
+                        Some(ref left)
+                            if u.as_ref()
+                                .map(|u| Rc::ptr_eq(u, left))
+                                .filter(|b| b == &true)
+                                .is_some() =>
+                        {
+                            let result = self.remove_fix_case2(p);
+                            color = result.0;
+                            u = result.1;
+                            p = result.2;
+                        }
+                        None if u.is_none() => {
+                            let result = self.remove_fix_case2(p);
+                            color = result.0;
+                            u = result.1;
+                            p = result.2;
+                        }
+                        _ => {
+                            let result = self.remove_fix_case3(p);
+                            color = result.0;
+                            u = result.1;
+                            p = result.2;
+                        }
                     }
-                    Some(ref left) if Rc::ptr_eq(&u, left) => u = self.remove_fix_case2(u),
-                    _ => u = self.remove_fix_case3(u),
                 }
             }
         }
-        if !Rc::ptr_eq(&u, self.r.as_ref().unwrap()) {
-            let w = u
-                .parent
-                .borrow()
-                .as_ref()
-                .and_then(|p| p.upgrade())
-                .unwrap();
-            if w.right.borrow().as_ref().map(|right| *right.color.borrow()) == Some(Color::Red)
-                && w.left.borrow().as_ref().map(|left| *left.color.borrow()) == Some(Color::Black)
-            {
-                self.flip_left(&w);
+        if u.as_ref()
+            .map(|u| !Rc::ptr_eq(u, self.r.as_ref().unwrap()))
+            .filter(|b| b == &true)
+            .is_some()
+            || u.is_none()
+        {
+            let left = p.as_ref().and_then(|w| w.left.borrow().clone());
+            let right = p.as_ref().and_then(|w| w.right.borrow().clone());
+            match (left, right) {
+                (Some(left), Some(right))
+                    if *right.color.borrow() == Color::Red
+                        && *left.color.borrow() == Color::Black =>
+                {
+                    self.flip_left(p.as_ref().unwrap())
+                }
+                (None, Some(right)) if *right.color.borrow() == Color::Red => {
+                    self.flip_left(p.as_ref().unwrap())
+                }
+                _ => (),
             }
         }
-        todo!()
     }
-    fn remove_fix_case1(&mut self, u: Rc<RBTNode<T>>) -> Rc<RBTNode<T>> {
-        let w = u.parent.borrow().as_ref().and_then(|p| p.upgrade());
-        if let Some(p) = &w {
-            self.flip_left(p);
-        }
-        u
+    fn remove_fix_case1(&mut self, u: Tree<T>, w: Tree<T>, color: isize) -> (isize, Tree<T>, Tree<T>) {
+        self.flip_right(w.as_ref().unwrap());
+        (color, u, w)
     }
-    fn remove_fix_case2(&mut self, u: Rc<RBTNode<T>>) -> Rc<RBTNode<T>> {
-        let w = u
-            .parent
-            .borrow()
-            .as_ref()
-            .and_then(|p| p.upgrade())
-            .unwrap();
-        let v = w.right.borrow().clone().unwrap();
-        Self::pull_black(&w);
-        self.flip_left(&w);
-        let q = w.right.borrow().clone().unwrap();
-        if *q.color.borrow() == Color::Red {
-            self.rotate_left(&w);
-            self.flip_right(&v);
-            Self::push_black(&q);
-            let vr = v.right.borrow().clone();
+    fn remove_fix_case2(&mut self, w: Tree<T>) -> (isize, Tree<T>, Tree<T>) {
+        let v = w.as_ref().and_then(|w| w.right.borrow().clone());
+        Self::pull_black(w.as_ref().unwrap()); // color -= 1;
+        self.flip_left(w.as_ref().unwrap());
+        let q = w.as_ref().and_then(|w| w.right.borrow().clone());
+        if q.as_ref().map(|q| *q.color.borrow()) == Some(Color::Red) {
+            self.rotate_left(w.as_ref().unwrap());
+            self.flip_right(v.as_ref().unwrap());
+            Self::push_black(q.as_ref().unwrap());
+            let vr = v.as_ref().and_then(|v| v.right.borrow().clone());
             if vr.as_ref().map(|vr| *vr.color.borrow()) == Some(Color::Red) {
-                self.flip_left(&v);
+                self.flip_left(v.as_ref().unwrap());
             }
-            q
+            let color = if let Some(q) = &q {
+                *q.color.borrow() as isize
+            } else {
+                1
+            };
+            let p = q
+                .as_ref()
+                .and_then(|q| q.parent.borrow().as_ref().and_then(|p| p.upgrade()));
+            (color, q, p)
         } else {
-            v
+            let color = if let Some(v) = &v {
+                *v.color.borrow() as isize
+            } else {
+                1
+            };
+            let p = v
+                .as_ref()
+                .and_then(|v| v.parent.borrow().as_ref().and_then(|p| p.upgrade()));
+            (color, v, p)
         }
     }
-    fn remove_fix_case3(&mut self, u: Rc<RBTNode<T>>) -> Rc<RBTNode<T>> {
-        let w = u
-            .parent
-            .borrow()
-            .as_ref()
-            .and_then(|p| p.upgrade())
-            .unwrap();
-        let v = w.left.borrow().clone().unwrap();
-        Self::pull_black(&w);
-        self.flip_right(&w);
-        let q = w.left.borrow().clone().unwrap();
-        if *q.color.borrow() == Color::Red {
-            self.rotate_right(&w);
-            self.flip_left(&v);
-            Self::push_black(&q);
-            q
-        } else {
-            let vl = v.left.borrow().clone();
-            if vl.as_ref().map(|vl| *vl.color.borrow()) == Some(Color::Red) {
-                Self::push_black(&v);
-                v
+    fn remove_fix_case3(&mut self, w: Tree<T>) -> (isize, Tree<T>, Tree<T>) {
+        let v = w.as_ref().and_then(|w| w.left.borrow().clone());
+        Self::pull_black(w.as_ref().unwrap());
+        self.flip_right(w.as_ref().unwrap());
+        let q = w.as_ref().and_then(|w| w.left.borrow().clone());
+        if q.as_ref().map(|q| *q.color.borrow()) == Some(Color::Red) {
+            self.rotate_right(w.as_ref().unwrap());
+            self.flip_left(v.as_ref().unwrap());
+            Self::push_black(q.as_ref().unwrap());
+            let color = if let Some(q) = &q {
+                *q.color.borrow() as isize
             } else {
-                self.flip_left(&v);
-                w
+                1
+            };
+            let p = q
+                .as_ref()
+                .and_then(|q| q.parent.borrow().as_ref().and_then(|p| p.upgrade()));
+            (color, q, p)
+        } else {
+            let vl = v.as_ref().and_then(|v| v.left.borrow().clone());
+            if vl.as_ref().map(|vl| *vl.color.borrow()) == Some(Color::Red) {
+                Self::push_black(v.as_ref().unwrap());
+                let color = if let Some(v) = &v {
+                    *v.color.borrow() as isize
+                } else {
+                    1
+                };
+                let p = v
+                    .as_ref()
+                    .and_then(|v| v.parent.borrow().as_ref().and_then(|p| p.upgrade()));
+                (color, v, p)
+            } else {
+                self.flip_left(v.as_ref().unwrap());
+                let color = if let Some(w) = &w {
+                    *w.color.borrow() as isize
+                } else {
+                    1
+                };
+                let p = w
+                    .as_ref()
+                    .and_then(|w| w.parent.borrow().as_ref().and_then(|p| p.upgrade()));
+                (color, w, p)
             }
         }
     }
@@ -343,6 +424,12 @@ impl<T: Ord + Clone> RedBlackTree<T> {
         let black_hight_min = result.1;
         let black_height_max = result.2;
         let left_leaning = result.3;
+        /*
+        println!(
+            "red_red={}, black_hight_min={}, black_height_max={}, left_leaning={}",
+            red_red, black_hight_min, black_height_max, left_leaning
+        );
+        */
         red_red == 0 && black_hight_min == black_height_max && left_leaning
     }
     fn validade(
@@ -352,7 +439,7 @@ impl<T: Ord + Clone> RedBlackTree<T> {
         black_height: usize,
     ) -> (usize, usize, usize, bool) {
         // black_height: min black-height == max black-height
-        // red_red: u.color + u.parent.color >= 1 
+        // red_red: u.color + u.parent.color >= 1
         // left-leaning: if u.left is black, u.right is black.
         if let Some(u) = node {
             let red_red = if parent_color == Color::Red && *u.color.borrow() == Color::Red {
@@ -364,7 +451,7 @@ impl<T: Ord + Clone> RedBlackTree<T> {
                 + match *u.color.borrow() {
                     Color::Black => 1,
                     Color::Red => 0,
-                    Color::WBlack => 2,
+                    Color::WBlack => panic!(),
                 };
             let left = &*u.left.borrow();
             let right = &*u.right.borrow();
@@ -409,30 +496,42 @@ where
     }
     fn remove(&mut self, x: &T) -> Option<T> {
         match self.find_last(x) {
-            Some(u) if &*u.x.borrow() == x => {
-                let mut w = u.right.borrow().clone();
-                let wi;
-                let ui;
+            Some(n) if &*n.x.borrow() == x => {
+                let mut u = Some(n);
+                let mut w = u.as_ref().and_then(|u| u.right.borrow().clone());
                 if w.is_none() {
-                    wi = u;
-                    ui = wi.left.borrow().clone()?;
+                    w = u;
+                    u = w.as_ref().and_then(|w| w.left.borrow().clone());
                 } else {
-                    while w.as_ref()?.left.borrow().is_some() {
-                        w = w.as_ref().and_then(|w| w.left.borrow().clone());
+                    loop {
+                        let mut next = None;
+                        if let Some(ref w) = w {
+                            match *w.left.borrow() {
+                                Some(ref left) => next = Some(left.clone()),
+                                None => break,
+                            }
+                        }
+                        w = next;
                     }
-                    wi = w?;
-                    u.x.swap(&wi.x);
-                    ui = wi.right.borrow().clone()?;
+                    u.as_ref().unwrap().x.swap(&w.as_ref().unwrap().x);
+                    u = w.as_ref().and_then(|w| w.right.borrow().clone());
                 }
-                let test = *ui.color.borrow() as isize + *wi.color.borrow() as isize;
-                *ui.color.borrow_mut() = match test {
-                    0 => Color::Red,
-                    1 => Color::Black,
-                    _ => Color::WBlack,
+                let p = w
+                    .as_ref()
+                    .and_then(|w| w.parent.borrow().as_ref().and_then(|p| p.upgrade()));
+                let color = if let (Some(u), Some(w)) = (&u, &w) {
+                    let c = *u.color.borrow() as isize + *w.color.borrow() as isize;
+                    *u.color.borrow_mut() = match c {
+                        0 => Color::Red,
+                        1 => Color::Black,
+                        _ => Color::WBlack,
+                    };
+                    c
+                } else {
+                    1 + *w.as_ref().unwrap().color.borrow() as isize
                 };
-                ui.parent.swap(&wi.parent);
-                let res = self.splice(wi);
-                self.remove_fixup(ui);
+                let res = self.splice(w.unwrap());
+                self.remove_fixup(color, u, p);
                 res
             }
             _ => None,
@@ -477,7 +576,52 @@ mod test {
         redblacktree.add(5);
         redblacktree.add(6);
         redblacktree.add(7);
+        assert!(!redblacktree.add(7));
         assert!(redblacktree.is_a_valid_red_black_tree());
         assert_eq!(redblacktree.size(), 7);
+        // find test
+        assert_eq!(redblacktree.find(&1), Some(1));
+        assert_eq!(redblacktree.find(&3), Some(3));
+        assert_eq!(redblacktree.find(&5), Some(5));
+        assert_eq!(redblacktree.find(&7), Some(7));
+        assert_eq!(redblacktree.find(&8), None);
+        // remove test
+        redblacktree.add(8);
+        redblacktree.add(9);
+        redblacktree.add(10);
+        redblacktree.add(11);
+        redblacktree.add(12);
+        redblacktree.add(13);
+        redblacktree.add(14);
+        redblacktree.add(15);
+        redblacktree.add(16);
+        redblacktree.add(17);
+        redblacktree.add(19);
+        redblacktree.add(20);
+        assert!(redblacktree.is_a_valid_red_black_tree());
+        assert_eq!(redblacktree.remove(&1), Some(1));
+        assert_eq!(redblacktree.remove(&3), Some(3));
+        assert_eq!(redblacktree.remove(&2), Some(2));
+        assert_eq!(redblacktree.remove(&6), Some(6));
+        assert!(redblacktree.is_a_valid_red_black_tree());
+        assert_eq!(redblacktree.remove(&5), Some(5));
+        assert_eq!(redblacktree.remove(&20), Some(20));
+        assert_eq!(redblacktree.remove(&9), Some(9));
+        assert_eq!(redblacktree.remove(&12), Some(12));
+        assert!(redblacktree.is_a_valid_red_black_tree());
+        assert_eq!(redblacktree.remove(&11), Some(11));
+        assert_eq!(redblacktree.remove(&18), None);
+        assert_eq!(redblacktree.remove(&17), Some(17));
+        assert_eq!(redblacktree.remove(&13), Some(13));
+        assert_eq!(redblacktree.remove(&12), None);
+        assert_eq!(redblacktree.remove(&4), Some(4));
+        assert!(redblacktree.is_a_valid_red_black_tree());
+        assert_eq!(redblacktree.remove(&7), Some(7));
+        assert_eq!(redblacktree.remove(&16), Some(16));
+        assert_eq!(redblacktree.remove(&14), Some(14));
+        assert_eq!(redblacktree.remove(&19), Some(19));
+        assert_eq!(redblacktree.remove(&8), Some(8));
+        //println!("{:#?}", redblacktree);
+        assert!(redblacktree.is_a_valid_red_black_tree());
     }
 }
