ba09f2b757fe8add55b2010bced128c98aae4539
ellbrid <ell@exoskeleton.dev>
2020-06-07 21:24:30 +0900

    fmt

--- chapter11/src/countingsort.rs
+++ chapter11/src/countingsort.rs
@@ -5,7 +5,7 @@ pub fn countingsort(a: &mut [usize]) {
         c[*ai] += 1;
     }
     for i in 1..k {
-        c[i] += c[i-1];
+        c[i] += c[i - 1];
     }
     let mut b = vec![0usize; a.len()];
     for ai in a.iter().rev() {
@@ -17,14 +17,17 @@ pub fn countingsort(a: &mut [usize]) {
 
 #[cfg(test)]
 mod test {
+    use super::countingsort;
     use rand::distributions::Uniform;
     use rand::{thread_rng, Rng};
-    use super::countingsort;
     #[test]
     fn test_countingsort() {
         let mut a = [7, 2, 9, 0, 1, 2, 0, 9, 7, 4, 4, 6, 9, 1, 0, 9, 3, 2, 5, 9];
         countingsort(&mut a);
-        assert_eq!(&a, &[0, 0, 0, 1, 1, 2, 2, 2, 3, 4, 4, 5, 6, 7, 7, 9, 9, 9, 9, 9]);
+        assert_eq!(
+            &a,
+            &[0, 0, 0, 1, 1, 2, 2, 2, 3, 4, 4, 5, 6, 7, 7, 9, 9, 9, 9, 9]
+        );
         let mut rng = thread_rng();
         for _ in 0u32..50000u32 {
             let len: usize = rng.gen();
