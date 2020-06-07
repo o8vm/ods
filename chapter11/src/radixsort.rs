de26e149728109ea75c4463974a35a1f72f7e9cd
ellbrid <ell@exoskeleton.dev>
2020-06-07 21:25:05 +0900

    fmt

--- chapter11/src/radixsort.rs
+++ chapter11/src/radixsort.rs
@@ -1,18 +1,18 @@
-pub fn radixsort(a: &mut[usize]) {
+pub fn radixsort(a: &mut [usize]) {
     let d = 8;
     let w = 8 * std::mem::size_of::<usize>();
-    for p in 0..(w/d) {
-        let mut c = vec![0usize; 1<<d];
+    for p in 0..(w / d) {
+        let mut c = vec![0usize; 1 << d];
         let mut b = vec![0usize; a.len()];
         for ai in a.iter() {
-            c[(*ai >> d*p)&((1<<d)-1)] += 1;
+            c[(*ai >> d * p) & ((1 << d) - 1)] += 1;
         }
-        for i in 1..(1<<d) {
-            c[i] += c[i-1];
+        for i in 1..(1 << d) {
+            c[i] += c[i - 1];
         }
         for ai in a.iter().rev() {
-            c[(*ai>>d*p)&((1<<d)-1)] -= 1;
-            b[c[(*ai>>d*p)&((1<<d)-1)]] = *ai;
+            c[(*ai >> d * p) & ((1 << d) - 1)] -= 1;
+            b[c[(*ai >> d * p) & ((1 << d) - 1)]] = *ai;
         }
         a.copy_from_slice(&b);
     }
@@ -20,14 +20,17 @@ pub fn radixsort(a: &mut[usize]) {
 
 #[cfg(test)]
 mod test {
+    use super::radixsort;
     use rand::distributions::Uniform;
     use rand::{thread_rng, Rng};
-    use super::radixsort;
     #[test]
     fn test_radixsort() {
         let mut a = [7, 2, 9, 0, 1, 2, 0, 9, 7, 4, 4, 6, 9, 1, 0, 9, 3, 2, 5, 9];
         radixsort(&mut a);
-        assert_eq!(&a, &[0, 0, 0, 1, 1, 2, 2, 2, 3, 4, 4, 5, 6, 7, 7, 9, 9, 9, 9, 9]);
+        assert_eq!(
+            &a,
+            &[0, 0, 0, 1, 1, 2, 2, 2, 3, 4, 4, 5, 6, 7, 7, 9, 9, 9, 9, 9]
+        );
         let mut rng = thread_rng();
         for _ in 0u32..50000u32 {
             let len: usize = rng.gen();
