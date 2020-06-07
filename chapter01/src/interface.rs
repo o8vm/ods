421589cb8df57eedb46f43d72b06b148df24569f
ellbrid <ell@exoskeleton.dev>
2020-06-08 00:56:01 +0900

    rename

--- chapter01/src/interface.rs
+++ chapter01/src/interface.rs
@@ -34,6 +34,6 @@ pub trait Graph {
     fn add_edge(&mut self, i: usize, j: usize);
     fn remove_edge(&mut self, i: usize, j: usize);
     fn has_edge(&self, i: usize, j: usize) -> bool;
-    fn out_edge(&self, i: usize) -> Vec<usize>;
-    fn in_edge(&self, i: usize) -> Vec<usize>;
+    fn out_edges(&self, i: usize) -> Vec<usize>;
+    fn in_edges(&self, i: usize) -> Vec<usize>;
 }
